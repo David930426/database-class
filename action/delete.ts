149a53b1b99dab8d1eee1a9e197b90285f596e1d
David Gunawan W <147725115+David930426@users.noreply.github.com>
2025-11-17 11:36:37 +0800

    make delete account function

---
+++ action/delete.ts
@@ -0,0 +1,31 @@
+"use server";
+import { DbConnect } from "@/lib/db";
+import { decrypt } from "@/lib/session";
+import { cookies } from "next/headers";
+import sql from "mssql";
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
+
+export async function deleteUser() {
+  try {
+    const cookieStore = await cookies()
+    const cookie = cookieStore.get("session")?.value;
+    const session = await decrypt(cookie);
+
+    const pool = await DbConnect();
+    const result = await pool
+      .request()
+      .input("userId", sql.Int, session?.userId)
+      .query(`DELETE FROM Users WHERE UserID = @userId`);
+    if (result.rowsAffected[0] === 0) {
+      console.log("Can not delete User");
+    }
+    cookieStore.delete("session");
+  } catch (err) {
+    const error = err as Error;
+    console.error(error.message);
+  }
+
+  revalidatePath("/login");
+  redirect("/login");
+}
--- app/edit/page.tsx
+++ app/edit/page.tsx
@@ -1,17 +1,33 @@
 "use client";
+import { deleteUser } from "@/action/delete";
 import { editProfile } from "@/action/edit";
 import { getData } from "@/action/getData";
 import { LoadingPage } from "@/components/loading";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
 import { ButtonPrimary } from "@/components/ui/button";
 import { Input } from "@/components/ui/form";
+import { Spinner } from "@/components/ui/spinner";
 import { GetData } from "@/lib/definitions";
 import { initialState } from "@/lib/initialState";
+import { TrashIcon } from "@heroicons/react/24/outline";
 import Link from "next/link";
 import { useActionState, useEffect, useState } from "react";
+import { useFormStatus } from "react-dom";
 
 export default function Page() {
   const [data, setData] = useState<GetData | null>(null);
   const [state, editProfileAction] = useActionState(editProfile, initialState);
+  const { pending } = useFormStatus();
   useEffect(() => {
     const dataGet = async () => {
       const dataFromDb = await getData();
@@ -32,9 +48,31 @@ export default function Page() {
           {"<"}
         </Link>
       </p>
-      <h1 className="bg-linear-to-r from-sky-400 via-sky-500 to-blue-500 p-7 rounded-xl text-3xl text-zinc-100 font-bold">
-        Edit Profile
-      </h1>
+      <div className="bg-linear-to-r from-sky-400 via-sky-500 to-blue-500 p-7 pr-10 items-center rounded-xl flex justify-between">
+        <h1 className="text-3xl text-zinc-100 font-bold">Edit Profile</h1>
+        <AlertDialog>
+          <AlertDialogTrigger asChild>
+            <TrashIcon className="size-11 p-2 text-rose-500 rounded-full hover:bg-zinc-100 hover:cursor-pointer hover:text-rose-600 active:text-rose-700 active:bg-zinc-200" />
+          </AlertDialogTrigger>
+          <AlertDialogContent>
+            <AlertDialogHeader>
+              <AlertDialogTitle>Delete Account</AlertDialogTitle>
+              <AlertDialogDescription>
+                Are you sure want to delete your <span>{data.Username}</span>{" "}
+                account?
+              </AlertDialogDescription>
+            </AlertDialogHeader>
+            <AlertDialogFooter>
+              <AlertDialogCancel disabled={pending} className="hover:cursor-pointer">Cancel</AlertDialogCancel>
+              <form>
+                <AlertDialogAction className="bg-rose-500 hover:bg-rose-600 active:bg-rose-700 w-full cursor-pointer" formAction={deleteUser} type="submit" disabled={pending}>
+                  {pending ? <Spinner className="size-6" /> : "Sure"}
+                </AlertDialogAction>
+              </form>
+            </AlertDialogFooter>
+          </AlertDialogContent>
+        </AlertDialog>
+      </div>
       <form action={editProfileAction} className="px-2">
         <Input
           type="text"
